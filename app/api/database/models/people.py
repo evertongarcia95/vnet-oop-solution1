3a72061086a6651a35b80e5c2d05484c04c62695
Douglas Bezerra Beniz <douglasbeniz@yahoo.com.br>
2021-07-23 21:36:43 +0200

    Updating comments of get/set methods;

--- app/api/database/models/people.py
+++ app/api/database/models/people.py
@@ -13,29 +13,37 @@ class Person:
         self.phoneNumber: int           = phoneNumber
         self.birthDate: datetime.date   = birthDate
 
-    def setName(self, name: str, surname: str) -> None:
-        self.name = name
-        self.surname = surname
+    def setFullName(self, name: str = None, surname: str = None) -> None:
+        """ Update full name of this person """
+        if name:    self.name       = name
+        if surname: self.surname    = surname
 
-    def getName(self) -> str:
+    def getFullName(self) -> str:
+        """ Obtain full name of this person """
         return self.name + " " + self.surname
 
     def setAddress(self, address: str) -> None:
+        """ Update the address of this person """
         self.address = address
     
     def getAddress(self) -> str:
+        """ Obtain the address of this person """
         return self.address
 
     def setPhoneNumber(self, phoneNumber: int) -> None:
+        """ Update the phone number of this person """
         self.phoneNumber = phoneNumber
 
     def getPhoneNumber(self) -> str:
+        """ Obtain the phone number of this person """
         return self.phoneNumber
 
-    def setBirthDate(self, birthDate: datetime.date) -> None: 
+    def setBirthDate(self, birthDate: datetime.date) -> None:
+        """ Update the birth date of this person """
         self.birthDate = birthDate
     
     def getBirthDate(self) -> datetime.date:
+        """ Obtain the birth date of this person """
         return self.birthDate
 
 
@@ -47,9 +55,11 @@ class Customer(Person):
         self.RegisterDate: datetime.date    = RegisterDate
 
     def setRegisterDate(self, RegisterDate: datetime.date) -> None:
+        """ Update the register date of this customer """
         self.RegisterDate = RegisterDate
 
     def getRegisterDate(self) -> datetime.date:
+        """ Obtain the register date of this customer """
         return self.RegisterDate
 
 
@@ -61,7 +71,9 @@ class Seller(Person):
         self.AdmissionDate: datetime.date = AdmissionDate
 
     def setAdmissionDate(self, AdmissionDate: datetime.date) -> None:
+        """ Update the admission date of this customer """
         self.AdmissionDate = AdmissionDate
 
     def getAdmissionDate(self) -> datetime.date:
+        """ Obtain the admission date of this customer """
         return self.AdmissionDate
